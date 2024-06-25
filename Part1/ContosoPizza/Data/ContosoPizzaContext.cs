704f0abfdce0771d7423b30e89a88c39e303d87d
Muhammad Bilal Mohib-ul-Nabi <2019cs682@student.uet.edu.pk>
2024-06-25 05:32:12 +0500

    feat

---
+++ Part1/ContosoPizza/Data/ContosoPizzaContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContosoPizza.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoPizza.Data
+{
+public class ContosoPizzaContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-DBT9PRJ;Initial Catalog=ContosoPizza-Part1;Integrated Security=True;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ Part1/ContosoPizza/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ContosoPizza.Data;
+using ContosoPizza.Models;
+
+using ContosoPizzaContext context = new ContosoPizzaContext();
+
+Product veggieSpecial = new Product()
+{
+    Name = "Veggie Special Pizza",
+    Price = 9.99M
+};
+Console.WriteLine("Going to Add the Product : " + veggieSpecial.Name);
+context.Products.Add(veggieSpecial);
+Console.WriteLine("Wow! Our Product " + veggieSpecial.Name + " is Added Successfully!");
+
+Product deluxeMeat = new Product()
+{
+    Name = "Deluxe Meat Pizza",
+    Price = 12.99M
+};
+Console.WriteLine("Going to Add the Product : " + deluxeMeat.Name);
+context.Add(deluxeMeat);
+Console.WriteLine("Wow! Our Product " + deluxeMeat.Name + " is Added Successfully!");
+context.SaveChanges();
